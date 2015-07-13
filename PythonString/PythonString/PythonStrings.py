8c563b5db9f482d83c855c4a8b7bb4069869502f
hello7s <strugglemonkey@gmail.com>
2015-07-13 17:14:19 -0400

    Python scripts
    Scripts for the Python tutorial

---
+++ PythonString/PythonString/PythonStrings.py
@@ -0,0 +1,34 @@
+city = "Boston"
+
+city = 'Boston'
+
+print(city)
+
+print("city[0]",city[0])
+
+print("city[1:6]",city[1:6])
+
+print(city + ", City of Champions")
+
+trophy = ", City of Champions"
+
+print(city + trophy)
+
+print(city + "\n" + trophy)
+
+print (city + r"\n")
+
+print ("%s is the city of champions" % city)
+
+print(city.find('s'))
+
+print(city + trophy)
+
+print(city.replace("Boston", "New York") + trophy)
+
+cities="Boston, New York, Chicago, Los Angeles"
+
+print(cities.split(","))
+
+
+
