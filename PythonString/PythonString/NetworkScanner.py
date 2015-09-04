d9f066ac64b35893762483f2599bd6c165bfd64f
hello7s <strugglemonkey@gmail.com>
2015-09-04 09:44:27 -0400

    Network Scanner
    Updated to include Nmap Network Scanner in Python

---
+++ PythonString/PythonString/NetworkScanner.py
@@ -0,0 +1,15 @@
+﻿#!/usr/bin/env python
+import nmap
+nm = nmap.PortScanner()         # Creates your nmap.PortScanner object
+nm.scan(hosts='10.0.0.0/24', arguments='-v -O -A') #Scanning your internal network range, in this case 10.0.0.0 /24 Subnetwork
+nm.command_line() 
+nm.all_hosts()                      # get all hosts that were scanned
+
+
+print('----------------------------------------------------')
+ # If you want to do a pingsweep on network 192.168.1.0/24:
+
+hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
+for host, status in hosts_list:
+     print('{0}:{1}'.format(host, status))
+
