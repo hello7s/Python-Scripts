bdaeb3be9736a5387bf16a1668c2ddb385ecf213
hello7s <strugglemonkey@gmail.com>
2017-06-21 14:02:47 -0400

    Updated intro python materials

---
+++ P4K/StringsIntro.py
@@ -0,0 +1,24 @@
+#Basic Introduction to Strings
+
+FourC = "4C is the place 4 me!"
+print(FourC)
+
+#Escaping with single quotes
+singleEscape = 'Cape Cod is "the place we can\'t help but love!"'
+print(singleEscape)
+
+#Escaping with double quotes
+doubleEscape = "Cape Cod is \"the place we can't help but double love!\""
+print(doubleEscape)
+
+#Insert values into strings
+myGrade = 100
+display = "I got a total of %s on my test!"
+print(display % myGrade)
+#Change variable from myGrade to yourGrade
+yourGrade = 80
+print(display % yourGrade)
+
+#Strings and multiplication
+print(20 * "ha ")
+
